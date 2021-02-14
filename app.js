597116b4598c2d13764db0203be035ea74fcb3d3
priya-pri <35191454+priya-pri@users.noreply.github.com>
2021-02-14 23:48:38 +0530

    commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express=require('express')
-const connectDB=require('./config/db')
+const connectDB=require('./Config/db')
 const ProductsRoute=require('./Routes/Products.route')
 const app=express()
 app.use(express.json());
